395c977cebe955dd93d66801327759be6ce39111
MaggieDeSantis <sheisntreal@gmail.com>
2023-04-01 05:38:24 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers =['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (arr, str) {
+    let drivers = arr.filter((item) => str.toUpperCase() === item.toUpperCase());
+    return drivers ? drivers : [];
+}
+
+function fuzzyMatch (arr, letter) {
+    return arr.filter ((item) => item.startsWith(letter));
+
+}
+function matchName (arr, name) {
+    return arr.filter ((item) => item.name === name);
+
+}
